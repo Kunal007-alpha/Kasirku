3cc27dc828f1b601472abf74333766513b0f8a52
rezadrian01 <ahmadadrian324@gmail.com>
2025-08-18 11:40:57 +0700

    feat: Implement CustomerController and update home route for product listing

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\PrintController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+Route::get('/', [CustomerController::class, 'index'])->name('home');
 
+// !important: This route will be using middleware 'auth' and 'verified' in the admin prefix group
 Route::get('/print', [PrintController::class, 'index'])->name('print.index');
 
 Route::prefix('admin')->middleware(['auth', 'verified'])->group(function () {
